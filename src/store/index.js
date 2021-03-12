b945f2c80e0f1e8f98a3ef16b91060a919ef753a
Yusuke Takei <yusuketakei@YusukenoiMac.local>
2021-03-12 11:28:55 +0900

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,47 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axiosBase from "axios";
+
+const axios = axiosBase.create({
+  baseURL: "https://qiita.com/api/v2"
+});
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    token: "",
+    items: []
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+    setItems(state, items) {
+      state.items = items;
+    },
+    setItem(state, item) {
+      state.item = item;
+    }
+  },
+  actions: {
+    async postItems(_, params) {
+      await axios.post("/items", params);
+    },
+    async getItems({ commit }) {
+      const response = await axios.get("/authenticated_user/items");
+      commit("setItems", response.data);
+    },
+    async getItem({ commit }, itemId) {
+      const response = await axios.get(`/items/${itemId}`);
+      commit("setItem", response.data);
+    },
+    async patchItem(_, { itemId, params }) {
+      await axios.patch(`/authenticated_user/items/${itemId}`, params);
+    },
+    async deleteItem(_, itemId) {
+      await axios.delete(`/authenticated_user/items/${itemId}`);
+    }
+  },
   modules: {}
 });
